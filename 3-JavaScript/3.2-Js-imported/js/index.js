7179b35bf47ad9d63f1873cc9f8cb6d5e2202914
testusuario <gabrielalvarez010100@gmail.com>
2023-05-09 15:56:58 -0600

    best practices

--- 3-JavaScript/3.2-Js-imported/js/index.js
+++ 3-JavaScript/3.2-Js-imported/js/index.js
@@ -1,3 +1,11 @@
+//mostrar mensaje en consola
 connsole.log("Hola");
+/*Definir e inicializar una variable*/
 var FatherName="Gabriel";
 console.log(FatherName);
+
+//definir variable=crearla
+var motherName;
+
+//inicializar una variable
+motherName="Anahi";
---
+++ 3-JavaScript/3.2-Js-imported/js/tipos-datos.js
@@ -0,0 +1,34 @@
+// tipos de datos
+
+// Primitivos
+//     Numericos(int y double)   10 y 0.8
+//     String  "hola mundo"
+//     Boolean TRUE FLASE
+//     Char H
+// Complejos
+//     Objetos {}
+//     Arreglos []
+
+/* Fuertemente tipado
+El lenguaje requiere saber el tipo de dato al momento 
+de crear una variable
+
+Ejemplo(java)
+int numero=10;
+
+Debilmente tipado
+Significa que el lenguaje interpreta el tipo de dato y 
+no requiere que se defina explicitamente 
+Ejemplo(JS)
+var number=6; 
+*/
+
+
+var numberSix=6;
+var decimalNumber=22.75;
+var myName="Gabriel";
+var myInitial="G";
+var flag= true;
+//flag esto esta OK
+flag = 22.72;
+flag="juanito banana";
