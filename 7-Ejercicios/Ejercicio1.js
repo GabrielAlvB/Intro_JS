bbfced13fdee14727928f186dd0415424fb0fbad
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-06-14 19:38:22 -0600

    using some logic

--- 7-Ejercicios/Ejercicio1.js
+++ 7-Ejercicios/Ejercicio1.js
@@ -1,10 +1,35 @@
 let respuesta = '';
 while (respuesta !== 'si' && respuesta !== 'no') {
-  respuesta = prompt('Por favor, ingresa "si" o "no":').toLowerCase();
+  respuesta = prompt('¿Eres bellisimo?, si o no').toLowerCase();
 }
 
 if (respuesta === 'si') {
-  console.log('El usuario ha respondido sí.');
+  alert('Ciertamente');
 } else {
-  console.log('El usuario ha respondido no.');
+  alert('No te creo');
 }
+
+//Ejercicio 2
+//Solicitar al usuario un número, y determinar si es divisible entre dos o no. 
+//Mostrando al usuario un mensaje de “x número es divisible entre 2” o “x núm no es divisible entre 2”.
+//3. Crear un programa que determine si un número introducido en un Prompt es par o no, 
+//la respuesta será mostrada en un Alert.
+
+respuesta = '';
+respuesta = parseFloat(prompt('Ingrese un numero para ver si es divisible'));
+
+if (respuesta%2 === 0) {
+    alert('Es par');
+  } else {
+    alert('No es par');
+  }
+//4. Solicitar al usuario un número de cliente. Si el número es el 1000, 
+//imprimir 'Ganaste un premio', en caso contrario mostrar el número y el mensaje “Lo sentimos, sigue participando”.
+respuesta = '';
+respuesta = parseInt(prompt('Ingrese numero de cliente'));
+
+if (respuesta === 1000) {
+    alert('Ganaste un premio');
+  } else {
+    alert('Sigue participando');
+  }
