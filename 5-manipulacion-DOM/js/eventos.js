8c05c025c45ac4dd8bfa7721255be89a7c3c94a5
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-06-20 17:10:30 -0600

    using events on images and inputtext

--- 5-manipulacion-DOM/js/eventos.js
+++ 5-manipulacion-DOM/js/eventos.js
@@ -5,3 +5,24 @@ document.getElementById("miBoton").addEventListener("click",
         document.getElementById("miBoton").style.background="red";
     }
 )
+
+//evento de cambio a una etiqueta input
+document.getElementById("inputText").addEventListener("change",
+function(){
+    alert("El valor ha cambiado");
+}
+)
+
+//evento de teclado en un input
+document.getElementById("inputText2").addEventListener("keyup",
+function(){
+    alert("Presionaste la tecla: "+ event.key);
+}
+)
+
+// evento de carga de una imagen
+document.getElementById("imagen1").addEventListener("load",
+function(){
+    alert("Se cargo la imagen");
+}
+)
