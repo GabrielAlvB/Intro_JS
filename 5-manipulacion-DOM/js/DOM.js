3fa09bbb033c6383b7262bdc3e0b9bb6eeeef5be
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-06-20 14:39:11 -0600

    using events on buttons

--- 5-manipulacion-DOM/js/DOM.js
+++ 5-manipulacion-DOM/js/DOM.js
@@ -1,11 +1,16 @@
 //cambniando el contenido
 
 document.getElementById("titulo").innerHTML = "Este es otro texto";
+//cambio de estilo
 document.getElementById("boton").style.background="red";
 //document.getElementById("boton").innerHTML ="red";
 
+//agregar un elemento a la pagina
 var nuevoParrafo=document.createElement("p");
 nuevoParrafo.setAttribute("id","miNuevoParrafo.");
 nuevoParrafo.innerHTML="este es mi nuevo parrafo";
 document.body.appendChild(nuevoParrafo);
 
+//eliminar un elemento de nuestra pagina
+var parrafoAEliminar=document.getElementById("parrafoAEliminar");
+parrafoAEliminar.parentNode.removeChild(parrafoAEliminar);
---
+++ 5-manipulacion-DOM/js/eventos.js
@@ -0,0 +1,7 @@
+//evento con boton 
+document.getElementById("miBoton").addEventListener("click",
+    function(){
+        alert("Hiciste click xD");
+        document.getElementById("miBoton").style.background="red";
+    }
+)
