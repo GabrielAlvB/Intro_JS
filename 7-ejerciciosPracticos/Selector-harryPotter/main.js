2951e1ed5eec0eb5067180d0b0c1dabe993a8868
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-06-25 12:04:11 -0600

    normalizing data from json

--- 7-ejerciciosPracticos/Selector-harryPotter/main.js
+++ 7-ejerciciosPracticos/Selector-harryPotter/main.js
@@ -3,6 +3,20 @@ const ulElement=document.querySelector('#contacts-ul');
 //declaramos vacio el array como buena practica
 let contacts = [];
 
-fetch("api/characters.json") //obtener informacion de una api
-.then(response=> response.json())
-.then(data=>console.log(data))
+function getDataFromJson(){
+    fetch("api/characters.json") //obtener informacion de una api
+    .then(response=> response.json())
+    .then(data=>normalizeData(data))
+    .then(cleanData=> console.log(cleanData))
+}
+
+//concepto frontend
+//normalizacion:  consiste en limpiar la info
+// que nos llega y tener solo la que necesitamos
+// y con la estructura
+
+function normalizeData(data){
+for(let i=0; i<data.lenght; i++)
+   console.log(data[i]); 
+}
+getDataFromJson();
